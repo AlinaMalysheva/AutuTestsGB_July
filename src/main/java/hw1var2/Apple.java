1f1ae08580df9e010a8eb72b67aa61f929441677
AlinaMalysheva <deiza@rambler.ru>
2020-07-15 00:25:52 +0300

    переделала класс Box, добавив дженерики

--- src/main/java/hw1var2/Apple.java
+++ src/main/java/hw1var2/Apple.java
@@ -1,8 +1,7 @@
 package hw1var2;
 
 public class Apple extends Fruit{
-    String fruitType = "Apple";
-    Float weight=1.0f;
+
     Apple() {
         super("Apple", 1.0f);
     }
--- src/main/java/hw1var2/Orange.java
+++ src/main/java/hw1var2/Orange.java
@@ -1,8 +1,6 @@
 package hw1var2;
 
 public class Orange extends Fruit {
-    String fruitType = "Orange";
-    Float weight=1.5f;
 
     Orange() {
         super("Orange", 1.5f);
